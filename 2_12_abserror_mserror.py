5327e51a87048e66a83751f9d3d7b412e9e37c2e
Xu Ren <musicalxu@hotmail.com>
2020-01-31 11:11:17 +0400

    init

---
+++ 2_12_abserror_mserror.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+
+
+points = [
+        (2,-2),
+        (5,6),
+        (-4,-4),
+        (-7,1),
+        (8,14)
+        ]
+
+x = [p[0] for p in points]
+y = [p[1] for p in points]
+
+
+line = lambda x: 1.2*x+2
+
+yhat = [line(_x) for _x in x]
+
+#plt.scatter(x, y)
+#plt.scatter(x, yhat)
+#plt.show()
+
+def abserror(y, yhat):
+    errors = []
+    for a,b in zip(y, yhat):
+        errors.append(abs(a-b))
+
+    return sum(errors)/len(y)
+
+print(abserror(y, yhat))
+
